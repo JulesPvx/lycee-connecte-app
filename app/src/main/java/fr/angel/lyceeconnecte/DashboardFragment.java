443f40ae496669c71f236d79fee6060759541bb3
BuildTools <unconfigured@null.spigotmc.org>
2022-02-21 11:53:12 +0100

    Added user simple information to view.

--- app/src/main/java/fr/angel/lyceeconnecte/DashboardFragment.java
+++ app/src/main/java/fr/angel/lyceeconnecte/DashboardFragment.java
@@ -3,6 +3,8 @@ package fr.angel.lyceeconnecte;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +16,9 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.imageview.ShapeableImageView;
 import com.google.android.material.textview.MaterialTextView;
@@ -31,6 +36,7 @@ import fr.angel.lyceeconnecte.Adapters.NewsAdapter;
 import fr.angel.lyceeconnecte.Adapters.NoveltyAdapter;
 import fr.angel.lyceeconnecte.Models.MyThread;
 import fr.angel.lyceeconnecte.Models.Novelty;
+import fr.angel.lyceeconnecte.Models.User;
 import fr.angel.lyceeconnecte.Utility.JsonFromUrl;
 import fr.angel.lyceeconnecte.Utility.ParseStringToJson;
 
@@ -46,6 +52,8 @@ public class DashboardFragment extends Fragment {
     private ShapeableImageView profilePictureImg;
     private MaterialTextView displayNameTv, schoolNameTv;
 
+    private User currentUser = new User();
+
     private final ArrayList<MyThread> threads = new ArrayList<>();
     private NewsAdapter newsAdapter;
     private final ArrayList<Novelty> novelties = new ArrayList<>();
@@ -58,8 +66,6 @@ public class DashboardFragment extends Fragment {
         // Setup SharedPreferences
         sharedPreferences = requireActivity().getSharedPreferences("data", Context.MODE_PRIVATE);
         editor = sharedPreferences.edit();
-
-
 
         oneSessionId = sharedPreferences.getString("oneSessionId", "");
 
@@ -104,6 +110,7 @@ public class DashboardFragment extends Fragment {
         // Try to put previously retrieved data into views
         String threadFeed = sharedPreferences.getString("threadFeed", "");
         String thread = sharedPreferences.getString("threads", "");
+        String user = sharedPreferences.getString("user", "");
         if (!threadFeed.trim().isEmpty()) {
             try {
                 parseToThreadFeed(Objects.requireNonNull(ParseStringToJson.parseStringToJsonArray(threadFeed)));
@@ -118,6 +125,13 @@ public class DashboardFragment extends Fragment {
                 if (threads.size() > 0) { newsCv.setVisibility(View.VISIBLE); }
             } catch (JSONException e) { e.printStackTrace(); }
         }
+        if (!user.trim().isEmpty()) {
+            try {
+                parseToUser(Objects.requireNonNull(ParseStringToJson.parseStringToJsonObject(user))); } catch (JSONException e) { e.printStackTrace(); }
+            displayNameTv.setText(currentUser.getDisplayName());
+            schoolNameTv.setText(currentUser.getStructureNames().get(0));
+        }
+
 
         if (status == MainActivity.STATUS_OK) {
             // Get data
@@ -127,6 +141,8 @@ public class DashboardFragment extends Fragment {
                     parseToThreads(Objects.requireNonNull(JsonFromUrl.getJsonArray("https://mon.lyceeconnecte.fr/actualites/infos/last/4", oneSessionId))); } catch (JSONException | IOException e) { e.printStackTrace(); }
                 try {
                     parseToThreadFeed(Objects.requireNonNull(JsonFromUrl.getJsonArray("https://mon.lyceeconnecte.fr/timeline/flashmsg/listuser", oneSessionId))); } catch (JSONException | IOException e) { e.printStackTrace(); }
+                try {
+                    parseToUser(Objects.requireNonNull(JsonFromUrl.getJsonObject("https://mon.lyceeconnecte.fr/auth/oauth2/userinfo", oneSessionId))); } catch (JSONException | IOException e) { e.printStackTrace(); }
 
                 requireActivity().runOnUiThread(() -> {
 
@@ -136,6 +152,41 @@ public class DashboardFragment extends Fragment {
                     if (threads.size() > 0) { newsCv.setVisibility(View.VISIBLE); }
                     if (novelties.size() > 0) { newsFeedCv.setVisibility(View.VISIBLE); }
 
+                    displayNameTv.setText(currentUser.getDisplayName());
+                    schoolNameTv.setText(currentUser.getStructureNames().get(0));
+
+                    // Display user profile picture
+                    Glide.with(this)
+                            .asBitmap()
+                            .load("https://mon.lyceeconnecte.fr/userbook/avatar/" + currentUser.getId())
+                            .into(new CustomTarget<Bitmap>() {
+                                @Override
+                                public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+
+                                    Bitmap image = resource;
+
+                                    Glide.with(requireActivity())
+                                            .asBitmap()
+                                            .load("https://mon.lyceeconnecte.fr/userbook/avatar/517cae4f-0c43-4ae4-b83f-6332536bb749")
+                                            .into(new CustomTarget<Bitmap>() {
+                                                @Override
+                                                public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                                                    if (!image.sameAs(resource)) {
+                                                        profilePictureImg.setImageBitmap(image);
+                                                    }
+                                                }
+
+                                                @Override
+                                                public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                                                }
+                                            });
+                                }
+
+                                @Override
+                                public void onLoadCleared(@Nullable Drawable placeholder) {
+                                }
+                            });
                 });
             }).start();
         }
@@ -176,6 +227,16 @@ public class DashboardFragment extends Fragment {
         }
 
         editor.putString("threads", data.toString());
+        editor.commit();
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private void parseToUser(JSONObject data) throws JSONException {
+
+        Gson gson = new Gson();
+        currentUser = gson.fromJson(data.toString(), User.class);
+
+        editor.putString("user", data.toString());
         editor.commit();
     }
 }
