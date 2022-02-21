08961d2d4710f06d588f305a44d42085337ab72f
BuildTools <unconfigured@null.spigotmc.org>
2022-02-21 10:47:37 +0100

    Update User model class

--- app/src/main/java/fr/angel/lyceeconnecte/Models/User.java
+++ app/src/main/java/fr/angel/lyceeconnecte/Models/User.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 
 public class User {
 
-    private String birthDate, externalId, federatedIDP, firstName, lastName, level, login, type, userId;
+    private String birthDate, externalId, federatedIDP, firstName, lastName, level, login, type, userId, address, email, health, id, mobile, mood, motto, photo, tel;
     @SerializedName(value = "displayName", alternate = "username")
     private String displayName;
     private Boolean deletePending, federated, hasApp, hasPw, needRevalidateTerms, forceChangePassword;
@@ -17,7 +17,7 @@ public class User {
 
     public User() { }
 
-    public User(String birthDate, String externalId, String federatedIDP, String firstName, String lastName, String level, String login, String type, String userId, String displayName, Boolean deletePending, Boolean federated, Boolean hasApp, Boolean hasPw, Boolean needRevalidateTerms, Boolean forceChangePassword, HashMap<String, String> children, HashMap<String, String> functions, ArrayList<Integer> childrenIds, ArrayList<String> classNames, ArrayList<String> classes, ArrayList<String> groupsIds, ArrayList<String> structureNames, ArrayList<String> structures) {
+    public User(String birthDate, String externalId, String federatedIDP, String firstName, String lastName, String level, String login, String type, String userId, String address, String email, String health, String id, String mobile, String mood, String motto, String photo, String tel, String displayName, Boolean deletePending, Boolean federated, Boolean hasApp, Boolean hasPw, Boolean needRevalidateTerms, Boolean forceChangePassword, HashMap<String, String> children, HashMap<String, String> functions, ArrayList<Integer> childrenIds, ArrayList<String> classNames, ArrayList<String> classes, ArrayList<String> groupsIds, ArrayList<String> structureNames, ArrayList<String> structures) {
         this.birthDate = birthDate;
         this.externalId = externalId;
         this.federatedIDP = federatedIDP;
@@ -27,6 +27,15 @@ public class User {
         this.login = login;
         this.type = type;
         this.userId = userId;
+        this.address = address;
+        this.email = email;
+        this.health = health;
+        this.id = id;
+        this.mobile = mobile;
+        this.mood = mood;
+        this.motto = motto;
+        this.photo = photo;
+        this.tel = tel;
         this.displayName = displayName;
         this.deletePending = deletePending;
         this.federated = federated;
@@ -42,6 +51,78 @@ public class User {
         this.groupsIds = groupsIds;
         this.structureNames = structureNames;
         this.structures = structures;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getMood() {
+        return mood;
+    }
+
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
+
+    public String getMotto() {
+        return motto;
+    }
+
+    public void setMotto(String motto) {
+        this.motto = motto;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
     }
 
     public String getBirthDate() {
