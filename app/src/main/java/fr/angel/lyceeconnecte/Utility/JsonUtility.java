b0d65fee49a98455cbb826399e0a4a9092a6a997
BuildTools <unconfigured@null.spigotmc.org>
2022-02-22 18:51:18 +0100

    Added puJsonObject function. Name from JsonFromUrl to JsonUtility.

--- app/src/main/java/fr/angel/lyceeconnecte/Utility/JsonUtility.java
+++ app/src/main/java/fr/angel/lyceeconnecte/Utility/JsonUtility.java
@@ -1,15 +1,19 @@
 package fr.angel.lyceeconnecte.Utility;
 
+import android.util.Log;
+
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 
-public class JsonFromUrl {
+public class JsonUtility {
 
     public static JSONObject getJsonObject(String strUrl, String oneSessionId) throws IOException {
         URL url = new URL(strUrl);
@@ -49,5 +53,25 @@ public class JsonFromUrl {
         responseStream.close();
 
         return jsonArray;
+    }
+
+    public static void putJsonObject(String strUrl, String oneSessionId, String payload) throws IOException, JSONException {
+        URL url = new URL(strUrl);
+        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+        httpConn.setRequestMethod("PUT");
+
+        httpConn.setRequestProperty("cookie", "oneSessionId=" + oneSessionId + ";");
+
+        httpConn.setDoOutput(true);
+        OutputStreamWriter writer = new OutputStreamWriter(httpConn.getOutputStream());
+        writer.write(payload);
+        writer.flush();
+        writer.close();
+        httpConn.getOutputStream().close();
+
+        // DO NOT DELETE: IT'S IMPORTANT FOR THE REQUEST
+        InputStream responseStream = httpConn.getResponseCode() / 100 == 2
+                ? httpConn.getInputStream()
+                : httpConn.getErrorStream();
     }
 }
