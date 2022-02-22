772b2db070cade1cd84873f812d62949611b372f
BuildTools <unconfigured@null.spigotmc.org>
2022-02-22 14:59:15 +0100

    Created new method to get user profile pic.

---
+++ app/src/main/java/fr/angel/lyceeconnecte/Utility/ProfilePicture.java
@@ -0,0 +1,51 @@
+package fr.angel.lyceeconnecte.Utility;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
+import com.google.android.material.imageview.ShapeableImageView;
+
+public class ProfilePicture {
+
+    public static void getUserProfilePicture(String id, Context context, ShapeableImageView img) {
+
+        Glide.with(context)
+                .asBitmap()
+                .load("https://mon.lyceeconnecte.fr/userbook/avatar/" + id)
+                .into(new CustomTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+
+                        Bitmap image = resource;
+
+                        Glide.with(context)
+                                .asBitmap()
+                                .load("https://mon.lyceeconnecte.fr/userbook/avatar/517cae4f-0c43-4ae4-b83f-6332536bb749")
+                                .into(new CustomTarget<Bitmap>() {
+                                    @Override
+                                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                                        if (!image.sameAs(resource)) {
+                                            img.setImageBitmap(image);
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                                    }
+                                });
+                    }
+
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) {
+                    }
+                });
+    }
+}
