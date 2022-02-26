b91d249f31cf511114cd85c0fbd3ad287688c4bf
BuildTools <unconfigured@null.spigotmc.org>
2022-02-26 13:30:15 +0100

    Simplified code.

--- app/src/main/java/fr/angel/lyceeconnecte/Utility/JsonUtility.java
+++ app/src/main/java/fr/angel/lyceeconnecte/Utility/JsonUtility.java
@@ -11,6 +11,7 @@ import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 public class JsonUtility {
