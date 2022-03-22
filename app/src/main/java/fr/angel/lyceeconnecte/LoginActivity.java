60556af71b03d996ee5fb55c7babc81269c397c6
Jules P <87655026+julespvx@users.noreply.github.com>
2022-03-22 21:05:56 +0100

    Login with TextInputLayout instead of WebView (Not completed).

--- app/src/main/java/fr/angel/lyceeconnecte/LoginActivity.java
+++ app/src/main/java/fr/angel/lyceeconnecte/LoginActivity.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.StrictMode;
+import android.util.Log;
 import android.webkit.CookieManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
@@ -15,12 +18,22 @@ import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
 
 import org.apache.commons.lang3.StringUtils;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import fr.angel.lyceeconnecte.Utility.JsonUtility;
 
 public class LoginActivity extends AppCompatActivity {
 
     private SharedPreferences.Editor editor;
+
+    private MaterialButton loginBtn;
+    private TextInputLayout loginTil, passwordTil;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -35,6 +48,9 @@ public class LoginActivity extends AppCompatActivity {
         // Bind views
         WebView webView = findViewById(R.id.login_web_view);
         MaterialButton startWithoutLoginBtn = findViewById(R.id.start_without_login_btn);
+        loginBtn = findViewById(R.id.login_btn);
+        loginTil = findViewById(R.id.login_til);
+        passwordTil = findViewById(R.id.login_password_til);
 
         startWithoutLoginBtn.setOnClickListener(v -> {
             // Start MainActivity
@@ -42,6 +58,38 @@ public class LoginActivity extends AppCompatActivity {
             i.putExtra("status", MainActivity.STATUS_NO_INTERNET);
             startActivity(i);
             finish();
+        });
+
+        // Set policy
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        // Handle login button
+        loginBtn.setOnClickListener(v -> {
+            String login = Objects.requireNonNull(loginTil.getEditText()).getText().toString().trim().toLowerCase();
+            String password = Objects.requireNonNull(passwordTil.getEditText()).getText().toString();
+            if (login.length() > 0 && password.length() > 0) {
+                try {
+                    JsonUtility.postJsonObject("https://educonnect.education.gouv.fr/idp/profile/SAML2/Unsolicited/SSO?execution=e9s1", null, "\"j_username=" + login + "&j_password=" + password + "&_eventId_proceed=\"");
+                } catch (IOException e) { e.printStackTrace(); }
+
+                String oneSessionId = null;
+                try {
+                    Log.e("TAG", "onCreate: " + JsonUtility.getJsonObject("https://mon.lyceeconnecte.fr/auth/oauth2/userinfo", "null").toString());
+                    oneSessionId = JsonUtility.getJsonObject("https://mon.lyceeconnecte.fr/auth/oauth2/userinfo", null).getJSONObject("sessionMetadata").getString("_id");
+                } catch (JSONException | IOException e) { e.printStackTrace(); }
+
+                // Save oneSessionId for later use
+                editor.putString("oneSessionId", oneSessionId);
+                editor.apply();
+
+                // Start MainActivity
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                i.putExtra("oneSessionId", oneSessionId);
+                i.putExtra("status", MainActivity.STATUS_OK);
+                startActivity(i);
+                finish();
+            }
         });
 
         // Setup web view
